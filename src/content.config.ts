78a554b67267f3a7191edf6a9ee38ab9b9c343be
thuanbui <thuanbui87@gmail.com>
2026-03-19 13:04:58 +0900

    feat: add Destinations component and integrate destination data in blog posts

--- src/content.config.ts
+++ src/content.config.ts
@@ -11,6 +11,7 @@ const blog = defineCollection({
     image: z.string().optional(),
     tags: z.array(z.string()).optional(),
     categories: z.array(z.string()).optional(),
+    destination: z.array(z.string()).optional(),
   }),
 });
 
